be31e0a8aad7acd7ca9cf0fe9362794d60b3fb3a
soyammangla <soyammangla15@gmail.com>
2026-01-13 14:25:07 +0530

    msg

--- extratask/6jan-import-export/server.js
+++ extratask/6jan-import-export/server.js
@@ -3,4 +3,4 @@ const os = require("os");
 console.log(os.arch());
 console.log(os.freemem() / 1024 ** 3);
 console.log(os.uptime() / 3600);
-console.log("totoal memory ", os.totalmem() / 1024 ** 3);
+console.log("total memory ", os.totalmem() / 1024 ** 3);
