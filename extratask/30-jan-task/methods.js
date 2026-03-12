a4cca76078fb1eaa0179a158f8c5e205e3a7e2a7
Soyam <soyammangla15@gmail.com>
2026-03-12 08:34:06 +0530

    Delete extratask directory

--- extratask/30-jan-task/methods.js
+++
@@ -1,9 +0,0 @@
-function demo() {
-  console.log("this is demo");
-}
-exports.fun = function () {
-  console.log("This is function expression");
-};
-exports.serve = () => {
-  console.log("this is arrow function");
-};
--- extratask/callback/tempcoderunnerfile.js
+++
@@ -1,3 +0,0 @@
-
-save(()=>{
-    console.log("hello")
