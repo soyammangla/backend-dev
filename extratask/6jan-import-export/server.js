00e09e7bc1c64a9dca4e66dbf31b049702910f11
Soyam <soyammangla15@gmail.com>
2026-03-20 08:30:45 +0530

    Delete extratask directory

--- extratask/6jan-import-export/server.js
+++
@@ -1,5 +0,0 @@
-const os = require("os");
-console.log(os.arch());
-console.log(os.freemem() / 1024 ** 3);
-console.log(os.uptime() / 3600);
-console.log("total memory ", os.totalmem() / 1024 ** 3);
