687cf32411ce5516011f072e189cd873dfa24bd3
Soyam <soyammangla15@gmail.com>
2026-04-30 00:17:04 +0530

    Delete middleware_ directory

--- middleware_/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { loginUser, logoutUser } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/login", loginUser);
-router.post("/logout", logoutUser);
-
-export default router;
